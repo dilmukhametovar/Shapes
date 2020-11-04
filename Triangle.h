fe769f93680404ab376b09fd49a0c32555c568d1
Dilmukhametov Artur <dilmukhametovar@gmail.com>
2020-11-04 16:52:14 +0300

    Fix Triangle constructor

--- Triangle.h
+++ Triangle.h
@@ -3,8 +3,8 @@
 class Triangle : public Shape {
 	double a, b, c; // triangle's sides in clockwise order
 public:
-	Triangle(double a) : Shape(), a(a), b(a), c(a) {}
-	Triangle(double x, double y, double a) {}
+	explicit Triangle(double a) : Shape(), a(a), b(a), c(a) {}
+	Triangle(double x, double y, double a) : Shape(x, y), a(a), b(a), c(a) {}
 	Triangle(double x, double y, double a, double b, double c) :
 		Shape(x, y), a(a), b(b), c(c) {}
 
