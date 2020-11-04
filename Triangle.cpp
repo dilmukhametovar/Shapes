c47711ffe1ecde9479348d582c96760c64ec7027
Dilmukhametov Artur <dilmukhametovar@gmail.com>
2020-11-04 16:14:20 +0300

    Create Triangle.cpp

---
+++ Triangle.cpp
@@ -0,0 +1,11 @@
+#include "Triangle.h"
+#include <iostream>
+
+void Triangle::draw() {
+	std::cout << "Drawing triangle with x = " << this->getX() <<
+		  ", y = " << this->getY() <<
+		  ", a = " << this->a <<
+		  ", b = " << this->b <<
+		  ", c = " << this->c << std::endl;
+}
+
