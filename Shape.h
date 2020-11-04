6e9aeb0c776d4c5da03c34b14cb22160b1761a68
Dilmukhametov Artur <dilmukhametovar@gmail.com>
2020-11-04 16:13:48 +0300

    Add getters to Shape class

--- Shape.h
+++ Shape.h
@@ -1,8 +1,14 @@
+#pragma once
+
 class Shape {
 	double x, y;
 public:
 	Shape() : x(0), y(0) {}
 	Shape(double x, double y) : x(x), y(y) {}
 	virtual ~Shape() {}
+
+	double getX() { return x; }
+	double getY() { return y; }
+
 	virtual void draw() = 0;
 };
