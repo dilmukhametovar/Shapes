52bfca125b1fb23ae184288e9f828390e3afd0c6
Dilmukhametov Artur <dilmukhametovar@gmail.com>
2020-11-04 16:51:46 +0300

    Fix Circle constructor

--- Circle.h
+++ Circle.h
@@ -4,7 +4,7 @@ class Circle : public Shape {
 	double radius;
 public:
 	explicit Circle(double radius) : Shape(), radius(radius) {}
-	Circle(double x, double y) : Shape(x, y), radius(radius) {}
+	Circle(double x, double y, double radius) : Shape(x, y), radius(radius) {}
 
 	virtual void draw() override;
 };
